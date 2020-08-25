dbd61ad52ba5f7fdf3cd4c4514af062ec7144feb
tourist <rickeyfsimple@gmail.com>
2020-08-26 00:47:54 +0600

    #1 Add separately insert doc dict and data dict

--- doc_dict/factory.py
+++ doc_dict/factory.py
@@ -5,18 +5,29 @@ from typing import Any, Dict, List
 from .datastructures import DocumentedValue
 
 
-def documented_dict(content: Dict[str, Any]) -> Dict[str, DocumentedValue]:
+def documented_dict(
+    content: Dict[str, Any], content_doc_dict: Dict[str, str] = None
+) -> Dict[str, DocumentedValue]:
     dict_: Dict[str, DocumentedValue] = {}
-
-    value_keys: List[str] = [
-        value_key for value_key in content if "_doc" not in value_key
-    ]
     value_key: str
+    documented_value: DocumentedValue
 
-    for value_key in value_keys:
-        documented_value: DocumentedValue = DocumentedValue(
-            value=content[value_key], doc=content.get(f"{value_key}_doc", "no docs")
-        )
-        dict_.update({value_key: documented_value})
+    if not content_doc_dict:
+        value_keys: List[str] = [
+            value_key for value_key in content if "_doc" not in value_key
+        ]
+
+        for value_key in value_keys:
+            documented_value = DocumentedValue(
+                value=content[value_key], doc=content.get(f"{value_key}_doc", "no docs")
+            )
+            dict_.update({value_key: documented_value})
+
+    elif content_doc_dict:
+        for value_key in content:
+            documented_value = DocumentedValue(
+                value=content[value_key], doc=content_doc_dict.get(value_key, "no docs")
+            )
+            dict_.update({value_key: documented_value})
 
     return dict_
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -13,3 +13,29 @@ def test_documented_dict():
     assert my_doc_dict["first_name"].doc == "It's name field"
     assert my_doc_dict["last_name"].value == "Anderson"
     assert my_doc_dict["last_name"].doc == "no docs"
+
+    my_dict = {
+        "first_name": "Mike",
+        "last_name": "Anderson",
+    }
+    my_dict_doc = {
+        "first_name": "It's name field",
+    }
+
+    my_doc_dict = documented_dict(my_dict, my_dict_doc)
+    assert my_doc_dict["first_name"].value == "Mike"
+    assert my_doc_dict["first_name"].doc == "It's name field"
+    assert my_doc_dict["last_name"].value == "Anderson"
+    assert my_doc_dict["last_name"].doc == "no docs"
+
+    my_doc_dict = documented_dict(my_dict, {})
+    assert my_doc_dict["first_name"].value == "Mike"
+    assert my_doc_dict["first_name"].doc == "no docs"
+    assert my_doc_dict["last_name"].value == "Anderson"
+    assert my_doc_dict["last_name"].doc == "no docs"
+
+    my_doc_dict = documented_dict(my_dict, {})
+    assert my_doc_dict["first_name"].value == "Mike"
+    assert my_doc_dict["first_name"].doc == "no docs"
+    assert my_doc_dict["last_name"].value == "Anderson"
+    assert my_doc_dict["last_name"].doc == "no docs"
