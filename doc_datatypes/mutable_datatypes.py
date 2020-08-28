812705e53f37acae20c9663cd76410fb13072257
tourist <rickeyfsimple@gmail.com>
2020-08-28 10:29:23 +0600

    #8 Fix doc. list bugs

--- doc_datatypes/mutable_datatypes.py
+++ doc_datatypes/mutable_datatypes.py
@@ -1,18 +1,36 @@
 import collections
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 from .datatypes import DocumentedIndex, DocumentedValue
 
 
 class DocumentedList(collections.abc.MutableSequence):
-    def __init__(self, content: list, docs_content: list = None):
+    def __init__(self, content: List[Any], docs_content: List[str] = None) -> None:
         self._inner_list = list()
         content, docs = self.__validate_docs_content(content, docs_content)
 
         for value, doc in zip(content, docs):
             self._inner_list.append(DocumentedIndex(value=value, doc=doc))
 
-    def __validate_docs_content(self, content: list, docs_content: list = None):
+    def __validate_docs_content(  # type: ignore
+        self, content: List[Any], docs_content: List[str] = None
+    ) -> Tuple[List[Any], List[str]]:
+        """Validate content & docs_content data
+
+        :param content: List of main content
+        :param docs_content: List of doc strings
+
+        if docs_content is None
+        :param content[counter][0]: Main content
+        :param content[counter][1]: Doc strings
+
+        if docs_content is not None
+        :return content: List[Any] -> Main content
+        :return docs_content: List[str] -> Doc strings
+
+        if docs_content is None
+        :return temp_content_arr: List[Any] -> Main content
+        :return temp_docs_arr: List[str] -> Doc strings"""
         if docs_content is not None:
             if len(content) >= len(docs_content):
                 for doc in range(len(content) - len(docs_content)):
@@ -23,8 +41,8 @@ class DocumentedList(collections.abc.MutableSequence):
                 raise ValueError("Content length less than docs content")
 
         elif docs_content is None:
-            temp_content_arr = []
-            temp_docs_arr = []
+            temp_content_arr: List[Any] = []
+            temp_docs_arr: List[str] = []
 
             for item in content:
                 if len(item) == 1:
@@ -32,20 +50,20 @@ class DocumentedList(collections.abc.MutableSequence):
                     temp_docs_arr.append("no docs")
                     continue
                 temp_content_arr.append(item[0])
-                temp_docs_arr.append(item[1])
+                temp_docs_arr.append(item[-1])
 
             return temp_content_arr, temp_docs_arr
 
-    def get_with_doc(self, index: int):
+    def get_with_doc(self, index: int) -> str:
         return f"{self._inner_list.__getitem__(index)}"
 
-    def get_doc(self, index: int):
+    def get_doc(self, index: int) -> str:
         return f"{self._inner_list.__getitem__(index).doc}"
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._inner_list)
 
-    def __delitem__(self, index):
+    def __delitem__(self, index: int) -> None:  # type: ignore
         self._inner_list.__delitem__(index)
 
     def insert(self, index: int, value: Any) -> None:
@@ -53,26 +71,24 @@ class DocumentedList(collections.abc.MutableSequence):
             value = [value, "no docs"]
         elif len(value) == 1:
             value.append("no docs")
-
         self._inner_list.insert(index, DocumentedIndex(value=value[0], doc=value[1]))
 
-    def __setitem__(self, index: int, value: Any):  # type: ignore
+    def __setitem__(self, index: int, value: Any) -> None:  # type: ignore
         if not isinstance(value, list):
             value = [value, "no docs"]
         elif len(value) == 1:
             value.append("no docs")
-
         self._inner_list.__setitem__(
             index, DocumentedIndex(value=value[0], doc=value[1])
         )
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Any:  # type: ignore
         return self._inner_list.__getitem__(index)()
 
-    def append(self, value: Any):
+    def append(self, value: Any) -> None:
         self.insert(len(self) + 1, value)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self._inner_list}"
 
 
