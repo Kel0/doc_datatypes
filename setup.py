ea964200efe722fee9ee4386e0a41fab4a845bc7
Kel0 <rozovdima123@gmail.com>
2020-09-02 20:36:46 +0600

    #15 Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="doc_datatypes",
-    version="0.0.4",
+    version="0.0.6",
     author="Example Author",
     author_email="rickeyfsimple@example.com",
     description="Dict with documentation _-_",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Kel0/doc_dict",
+    url="https://github.com/Kel0/doc_datatypes",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
